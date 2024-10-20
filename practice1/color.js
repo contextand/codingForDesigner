0a0beb6f42fcba04b4339ff24135ceda822ea31b
rayong <skdyds@gmail.com>
2024-10-20 14:01:09 +0900

    호버시 컬러 변경 중

--- practice1/color.js
+++ practice1/color.js
@@ -1,9 +1,21 @@
 const red = document.querySelector(".red");
+const blue = document.querySelector(".blue");
+const yellow = document.querySelector(".yellow");
 const background = document.querySelector(".background");
 
-const colorChange = (e) => {
-  console.log(e);
-  background.classList.add("red-change");
+const changeRed = (e) => {
+  // console.log(e.target.innerText);
+  background.classList.add("orangered");
+};
+const changeBlue = (e) => {
+  // console.log(e.target.innerText);
+  background.classList.add("dodgerblue");
+};
+const changeYellow = (e) => {
+  // console.log(e.target.innerText);
+  background.classList.add("greenyellow");
 };
 
-red.addEventListener("mouseover", colorChange);
+red.addEventListener("mouseover", changeRed);
+blue.addEventListener("mouseover", changeBlue);
+yellow.addEventListener("mouseover", changeYellow);
