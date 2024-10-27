ae9e57a840741dc49cda4bc013c4e65622fecf61
rayong <skdyds@gmail.com>
2024-10-27 16:10:11 +0900

    수업 복습 해야 하고, 호버시 이미지 나오게 하는 것도 해야함

--- practice1/color.js
+++ practice1/color.js
@@ -23,3 +23,7 @@ blue.addEventListener("mouseover", changeColor);
 blue.addEventListener("mouseout", changeColorBack);
 yellow.addEventListener("mouseover", changeColor);
 yellow.addEventListener("mouseout", changeColorBack);
+
+// 이미지를 미리 넣어두었다가, 오파시티를 조절하는 방식으로 접근한다.
+// 폰트는 베리어블 폰트 적용되는 폰트로, 두께 조절이 자연스럽게 되는 것은 해보아야 알 수 있음.
+// 글자 좌측에 화살표도 넣어두었다가, 스케일 조절하는 식으로 키울 수 있을 것
