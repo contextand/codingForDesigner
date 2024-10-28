c4dee1335bec79b4d29e72a3ce1e4e46f5b34b05
rayong <skdyds@gmail.com>
2024-10-28 16:26:47 +0900

    텍스트 변경하니 자연스럽게 됨

--- practice1/color.js
+++ practice1/color.js
@@ -8,13 +8,10 @@ const span = document.createElement("span");
 const changeColor = (e) => {
   background.classList.add(`${e.target.innerText}`);
   e.srcElement.classList.add("bold");
-  // e.srcElement.appendChild(span);
-  // span.innerText = " is..";
 };
 const changeColorBack = (e) => {
   background.classList.remove(`${e.target.innerText}`);
   e.srcElement.classList.remove("bold");
-  // e.srcElement.removeChild(span);
 };
 
 red.addEventListener("mouseover", changeColor);
