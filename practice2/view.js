ff4567cd73e5681b0b28a0d1d51e8914eeb2116d
rayong <skdyds@gmail.com>
2024-11-17 15:48:33 +0900

    정리 후 이전

--- practice2/view.js
+++ practice2/view.js
@@ -1,7 +1,15 @@
-const li = document.querySelector(".list");
+// const liAr = [...document.querySelectorAll(".text")];
 
-const viewText = (e) => {
-  console.log(e);
-};
+// const viewText = (e) => {
+//   console.log(e.target);
+// };
 
-li.addEventListener("click", viewText);
+// liAr.map((li) => {
+//   li.addEventListener("click", viewText);
+// });
+
+$(".text").click(function (e) {
+  const t = $(e.target).text();
+
+  $(".text_wrap p").text(t);
+});
